7b5e6d87dcdac41c5495d30c9243ee07cec7d2d6
Joshua Kissoon <joshuakissoon@Joshuas-MacBook-Pro.local>
2019-04-18 15:07:02 -0400

    Minor bug fixes

--- src/TechlifySimpleInventoryFacade.php
+++ src/TechlifySimpleInventoryFacade.php
@@ -15,7 +15,7 @@ class TechlifySimpleInventoryFacade extends Facade
 
     protected static function getFacadeAccessor()
     {
-        return 'laravel-model-logger';
+        return 'techlify-simple-inventory';
     }
 
 }
